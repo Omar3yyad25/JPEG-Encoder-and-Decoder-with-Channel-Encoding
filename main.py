0df8776f1b72faa30dedd38dbeab52bc727e2944
Omar W. Ayyad <202000806@zewailcity.edu.eg>
2023-12-01 01:43:20 +0200

    dct block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from PIL import Image
+from scipy.fftpack import dct, idct
 import matplotlib.pyplot as plt
 
 
@@ -25,9 +26,32 @@ def zigzag_indices ():
     indices.sort(key=lambda x: (x[0]+x[1], x[1]) if (x[0]+x[1]) % 2 == 0 else (x[1]+x[0], x[0]))
     return indices
 
+def built_in_dct(block):
+    return dct(dct(block.T, norm='ortho').T, norm='ortho')
+
+
+def dct_2d(block):
+    dct_block = np.zeros((8, 8))
+    for u in range(8):
+        for v in range(8):
+            sum = 0
+            for x in range(8):
+                for y in range(8):
+                    if x == 0 and y == 0:
+                        sum += block[x][y] * np.cos(((2 * x + 1) * u * np.pi) / 64) * np.cos(((2 * y + 1) * v * np.pi) / 64)
+                    elif x == 0 or y == 0:
+                        sum += block[x][y] * np.cos(((2 * x + 1) * u * np.pi) / 32) * np.cos(((2 * y + 1) * v * np.pi) / 32)
+                    else:
+                        sum += block[x][y] * np.cos(((2 * x + 1) * u * np.pi) / 16) * np.cos(((2 * y + 1) * v * np.pi) / 16)
+            dct_block[u][v] = sum
+    return dct_block
+
 image_path = 'image.jpeg'
 image = read_image(image_path)
 blocks = image_blocks(image)
 
+blocks[200] = built_in_dct(blocks[700])
 
-print (zigzag_indices())
+plt.imshow(blocks[700], cmap='gray')
+plt.axis('off')  
+plt.show()
