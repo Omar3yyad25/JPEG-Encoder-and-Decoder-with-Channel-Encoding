2e2bc61b81537cd1a2fe03d9d937c7a2b7c692aa
Omar W. Ayyad <202000806@zewailcity.edu.eg>
2023-12-03 14:53:25 +0200

    till dequantization step

--- decoder.py
+++ decoder.py
@@ -1,15 +1,41 @@
-def run_length_decoding(vectors):
-    decoded_vectors = []
-    for i in range(len(vectors)):
-        decoded_vector = []
-        j = 0
-        while j < len(vectors[i]):
-            if vectors[i][j] == 0:
-                for k in range(vectors[i][j+1]):
-                    decoded_vector.append(0)
-                j += 2 
-            else:
-                decoded_vector.append(vectors[i][j])
-                j += 1  
-        decoded_vectors.append(decoded_vector)
-    return decoded_vectors
+from encoder import zigzag_indices, Low_Compression_Quantizer, High_Compression_Quantizer
+import numpy as np
+
+def run_length_decoding(vector):
+    decoded_vector = []
+    j = 0
+    while j < len(vector):
+        if vector[j] == 0:
+            for k in range(vector[j+1]):
+                decoded_vector.append(0)
+            j += 2 
+        else:
+            decoded_vector.append(vector[j])
+            j += 1  
+        
+    return decoded_vector
+
+def convert_1d_to_2d(vectors):
+    zigzag = zigzag_indices()
+    vectors_2d = []
+    for i in range (len(vectors)):
+        block = np.zeros((8, 8))
+        for j in range (len(zigzag)):
+            block[zigzag[j][0]][zigzag[j][1]] = vectors[i][j]
+        vectors_2d.append(block)
+    return vectors_2d
+
+def Dequantization(blocks, QF):
+    if QF == '1':
+        for i in range (len(blocks)):
+            blocks[i] = Low_Compression_Quantizer * blocks[i]
+    elif QF == '2':
+        for i in range (len(blocks)):
+            blocks[i] = High_Compression_Quantizer * blocks[i]
+    
+    return blocks
+
+
+
+
+       
--- main.py
+++ main.py
@@ -9,57 +9,46 @@ def main():
     image_path = 'image.jpeg'
     image = read_image(image_path)
     blocks = image_blocks(image)
+    number_of_blocks = len(blocks)
     
     #Applying DCT to each block
     for i in range (len(blocks)):
         blocks[i] = dct_2d(blocks[i])
 
     #Quantization
-    Low_Compression_Quantizer = np.array(
-    [[1, 1, 1, 1, 1, 2, 2, 4],
-      [1, 1, 1, 1, 1, 2, 2, 4],
-      [1, 1, 1, 1, 2, 2, 2, 4],
-      [1, 1, 1, 1, 2, 2, 4, 8],
-      [1, 1, 2, 2, 2, 2, 4, 8],
-      [2, 2, 2, 2, 2, 4, 8, 8],
-      [2, 2, 2, 4, 4, 8, 8, 16],
-      [4, 4, 4, 4, 8, 8, 16, 16]
-    ])
-    
-    High_Compression_Quantizer= np.array([
-    [1, 2, 4, 8, 16, 32, 64, 128],
-    [2, 4, 4, 8, 16, 32, 64, 128],
-    [4, 4, 8, 16, 32, 64, 128, 128],
-    [8, 8, 16, 32, 64, 128, 128, 256],
-    [16, 16, 32, 64, 128, 128, 256, 256],
-    [32, 32, 64, 128, 128, 256, 256, 256],
-    [64, 64, 128, 128, 256, 256, 256, 256],
-    [128, 128, 128, 256, 256, 256,256,256]
-    ])
-
-    # Apply the Quantization step 
-    # Divide each each block by the quantization matrix
     compression = input("Please enter 1 for low compression and 2 for high compression")
-    if compression == '1':
-        for i in range (len(blocks)):
-            blocks[i] = np.round(blocks[i]/ Low_Compression_Quantizer)
-    elif compression == '2':
-        for i in range (len(blocks)):
-            blocks[i] = np.round(blocks[i]/ High_Compression_Quantizer)
-    else:
-        print ("Wrong input")
-        exit()
+    blocks = Quantization(blocks, compression)
     
     #using zigzag indices to flatten each matrix into a vector and store all vectors in a list
     vectors = convert_2d_to_1d(blocks)
+    print ("Vectors: ", vectors[1000])
     #Run length encoding
     encoded_vectors = run_length_encoding(vectors)
+    #Huffman encoding
     f = Huffman(list(chain.from_iterable(encoded_vectors)))
-
     encoded_data = huffman_encode(list(chain.from_iterable(encoded_vectors)) , f.table)
-    decoded_result = huffman_decode(encoded_data, f.table)
-    print(decoded_result)
 
-                
+    #Huffman decoding
+    decoded_result = huffman_decode(encoded_data, f.table)
+    #Run length decoding
+    decoded_vector = run_length_decoding(decoded_result)
+
+    #divide the decoded vector into 64 elements vectors
+    decoded_vectors = []
+    index = 0
+    for i in range (number_of_blocks):
+        decoded_vector_64 = []
+        for j in range (64):
+            decoded_vector_64.append(decoded_vector[index])
+            index += 1
+        decoded_vectors.append(decoded_vector_64)
+
+    #convert each vector into a 8x8 matrix
+    decoded_blocks = convert_1d_to_2d(decoded_vectors)
+
+    #Dequantization
+    decoded_blocks = Dequantization(decoded_blocks, compression)
+    print ("Decoded blocks: ", decoded_blocks[1000])
+
 if __name__ == '__main__':
     main()
